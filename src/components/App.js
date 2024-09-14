77cbaed7768daf61c512742479b7096426b28785
Create_first_repo <gauravchoundiye123@gmail.com>
2024-09-14 13:22:03 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import './../styles/App.css';
-const [name,setName]=useState("");
+const [name,setName]=useState();
 const App = () => {
   return (
-    <div id='main'>
-      {/* <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p> */}
-      <label htmlFor="">Enter Your Name:</label><br />
-      <input type="text" onChange={(e)=>{
-        setName(e.target.value);
-      }} /> <br />
-      <p>{name}</p>
-    </div>
+    <div>
+      
+    <label htmlFor='ip'>Enter your name:</label><br/>
+    <input id='ip' type='text' onChange={(e) => {
+      setText(e.target.value)
+    }} />
+    <p>{text && `Hello ${text}!`}</p>
+
+  </div>
   )
 }
 
