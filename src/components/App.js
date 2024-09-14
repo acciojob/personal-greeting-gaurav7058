f96b80d460690a4e89380ee74481888ab6ea61d5
Create_first_repo <gauravchoundiye123@gmail.com>
2024-09-14 13:28:31 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import './../styles/App.css';
-const [name,setName]=useState();
 const App = () => {
+  const [text, setText] = useState()
   return (
     <div>
-      
+
     <label htmlFor='ip'>Enter your name:</label><br/>
     <input id='ip' type='text' onChange={(e) => {
       setText(e.target.value)
